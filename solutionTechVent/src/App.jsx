c961f8723f751a5f92fe31f3a9c87082fbd470d1
denis88k <denis88.k@mail.ru>
2023-03-27 22:46:50 +0300

    feat: style 20%

--- solutionTechVent/src/App.jsx
+++ solutionTechVent/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
-// import reactLogo from './assets/img/bg.png'
+import logo from './assets/img/logo.png'
+import regular from './assets/img/section_regular.png'
+import fan from './assets/img/section_fan.png'
 
 
 function App() {
@@ -10,7 +12,7 @@ function App() {
         <div className='container'>
           <div className="header__inner">
             <div className='logo'>
-              <img className='logo__img' src="assets/img/logo.png" alt="logo" />
+              <img className='logo__img' src={logo} alt="logo" />
             </div>
           </div>
         </div>
@@ -30,69 +32,73 @@ function App() {
             </div>
 
             <div className='main__screen'>
-                {/* расчёт гибких вставок */}
+              {/* расчёт гибких вставок */}
               <div className='solution__GV'>
+                {/* расчёт в обычной секции */}
+                <section className='section section__regular'>
 
-                <div className='section section__regular'>
-                  <form className='form'>
-                    <h3 className='form__title'>Расчёт в обычной секции</h3>
-                    <label className='label'>
-                      Расход:
-                      <input className='input consumption' type="number"/>
-                    </label>
-                    <label className='label'>
-                      Ширина сечения:
-                      <input className='input witdh_section' type="number"/>
-                    </label>
-                    <label className='label'>
-                      Необходимая скорость:
-                      <input className='input required_speed' type="number" value="4"/>
-                    </label>
-                  </form>
-
-                  <div>
-                    Min длина <b>Гибкой вставки:</b>{`min длина`}
+                  <div className='form-block'>
+                    <form className='form'>
+                      <h3 className='form__title'>Расчёт в обычной секции</h3>
+                      <label className='form__label label'>
+                        Расход:
+                        <input className='form__input input consumption' type="number" />
+                      </label>
+                      <label className='form__label label'>
+                        Ширина сечения:
+                        <input className='form__input input width_section' type="number" />
+                      </label>
+                      <label className='form__label label'>
+                        Необходимая скорость:
+                        <input className='form__input input required_speed' type="number" defaultValue="4" />
+                      </label>
+                    </form>
+                    <div className='result'>
+                      Min длина <b>Гибкой вставки:</b>{` min длина`}
+                    </div>
                   </div>
 
                   <div className='img'>
-                    <img src="./assets/img/section_regular.png" alt="обычная секция"/>
+                    <img src={regular} alt="обычная секция" />
                   </div>
-                </div>
+                </section>
 
                 <hr />
 
-                <div className='section section__regular'>
+                {/* расчёт в секции вентилятора*/}
+                <section className='section section__regular'>
+
                   <form className='form'>
                     <h3 className='form__title'>Расчёт в секции вентилятора</h3>
                     <label className='label'>
                       Расход:
-                      <input className='input consumption' type="number"/>
+                      <input className='input consumption' type="number" />
                     </label>
                   </form>
-
                   <div>
                     Min длина Гибкой вставки:{`min длина`}
                   </div>
 
+
                   <div className='img'>
-                    <img src="./assets/img/section_fan.png" alt="Cекция вентилятора"/>
+                    <img src={fan} alt="Секция вентилятора" />
                   </div>
-                </div>
+                </section>
 
               </div>
 
-                {/* расчёт смесителя */}
-              <div>
+              {/* расчёт смесителя */}
+              {/* <div>
                 <div>
                   <form className='form'>
                     <h3 className='form__title'>Расчёт смесителя</h3>
                     <label className='label'>
                       Расход:
-                      <input  className='input ' type="number" />
+                      <input className='input ' type="number" />
                     </label>
                     <label className='label'>
                       Давление:
-                      <input  className='input ' type="number" />
+                      <input className='input ' type="number" />
                     </label>
                   </form>
                 </div>
@@ -104,7 +110,7 @@ function App() {
                   -при нажатии копируется смесак в буфер обмена
                   -выделяется полупрозрачным фоном наиболее оптимальные смесаки
                 </table>
-              </div>
+              </div> */}
 
             </div>
 
