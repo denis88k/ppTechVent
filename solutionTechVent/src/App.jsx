17632e3dd7c82a8e6d398b961648c2b0abbabc25
Denis(denis88k) <denis88.k@mail.ru>
2023-03-27 15:01:10 +0300

    feat: html

--- solutionTechVent/src/App.jsx
+++ solutionTechVent/src/App.jsx
@@ -5,67 +5,99 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <div className="App">
+    <div className="App bg">
       <header>
         <div className='container'>
           <div className="header__inner">
-
             <div className='logo'>
               <img className='logo__img' src="assets/img/logo.png" alt="logo" />
             </div>
-
-
-
           </div>
         </div>
       </header>
 
       <main>
-        <div className="container">
+        <div className='container'>
           <div className="main__inner">
 
             <div className='tabs'>
               <div className='tab tab-active'>
                 Расчёт гибкой вставки
               </div>
+              <div className='tab'>
+                Расчёт смесителя
+              </div>
             </div>
 
-            <div>
-              <div>
+            <div className='main__screen'>
                 {/* расчёт гибких вставок */}
-                <div>
-                  <form>
-                    <label>расчёт в обычной секции</label>
-                    <input type="text" />
+              <div className='solution__GV'>
+
+                <div className='section section__regular'>
+                  <form className='form'>
+                    <h3 className='form__title'>Расчёт в обычной секции</h3>
+                    <label className='label'>
+                      Расход:
+                      <input className='input consumption' type="number"/>
+                    </label>
+                    <label className='label'>
+                      Ширина сечения:
+                      <input className='input witdh_section' type="number"/>
+                    </label>
+                    <label className='label'>
+                      Необходимая скорость:
+                      <input className='input required_speed' type="number" value="4"/>
+                    </label>
                   </form>
+
                   <div>
-                    Min длина Гибкой вставки:{`min длина`}
+                    Min длина <b>Гибкой вставки:</b>{`min длина`}
                   </div>
-                  <img src="" alt="" />
+
+                  <div className='img'>
+                    <img src="./assets/img/section_regular.png" alt="обычная секция"/>
+                  </div>
                 </div>
 
-                <div>
-                  <form>
-                    <label>расчёт в секции вентилятора</label>
-                    <input type="text" />
+                <hr />
+
+                <div className='section section__regular'>
+                  <form className='form'>
+                    <h3 className='form__title'>Расчёт в секции вентилятора</h3>
+                    <label className='label'>
+                      Расход:
+                      <input className='input consumption' type="number"/>
+                    </label>
                   </form>
+
                   <div>
                     Min длина Гибкой вставки:{`min длина`}
                   </div>
-                  <img src="" alt="" />
+
+                  <div className='img'>
+                    <img src="./assets/img/section_fan.png" alt="Cекция вентилятора"/>
+                  </div>
                 </div>
+
               </div>
 
-              <div>
                 {/* расчёт смесителя */}
+              <div>
                 <div>
-                  <form>
-                    <label>расчёт смесителя</label>
-                    <input type="text" />
+                  <form className='form'>
+                    <h3 className='form__title'>Расчёт смесителя</h3>
+                    <label className='label'>
+                      Расход:
+                      <input  className='input ' type="number" />
+                    </label>
+                    <label className='label'>
+                      Давление:
+                      <input  className='input ' type="number" />
+                    </label>
                   </form>
                 </div>
 
-                <table>
+                <table className='table'>
                   таблица вывода
                   -выводится по всем смесакам
                   -при наведении на строку: выделяется область пунктирными линиями и появляется надпись скопировать
@@ -81,7 +113,7 @@ function App() {
       </main>
 
       <footer>
-        <div className="container">
+        <div className='container'>
           <div className="footer__inner">
             footer
           </div>
