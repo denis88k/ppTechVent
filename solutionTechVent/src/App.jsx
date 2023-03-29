cf209b9e67880280e4b849acdd474d0969e8aaae
denis88k <denis88.k@mail.ru>
2023-03-29 23:20:44 +0300

    feat section regular change minWidthGV

--- solutionTechVent/src/App.jsx
+++ solutionTechVent/src/App.jsx
@@ -9,25 +9,45 @@ function App() {
   const [consumption, setConsumption] = useState(1000)
   const [widthSection, setWidthSection] = useState(500)
   const [requiredSpeed, setRequiredSpeed] = useState(4)
-  const [resultRegularSection, setResultRegularSection] = useState('')
+  const [minWidthGV, setMinWidthGV] = useState(250)
+  const [focus, setFocus] = useState('speed')
 
-
-  const regularSection = ()=>{
-    const lengthMin = (consumption * 1000 / (3600 * requiredSpeed * (widthSection / 1000))).toFixed(0)
-    if(consumption && widthSection && requiredSpeed){
-      (lengthMin <= 250) 
-        ? setResultRegularSection('М = 250 мм')
-        : setResultRegularSection(`М = ${lengthMin} мм`)
+  const onMinWidthGV = () => {
+    if (+requiredSpeed === 0) {
+      return setMinWidthGV(0)
+    } else {
+      const lengthMin = (consumption * 1000 / (3600 * requiredSpeed * (widthSection / 1000))).toFixed(0)
+      if (consumption && widthSection && requiredSpeed) {
+        (+lengthMin <= 250)
+          ? setMinWidthGV(250)
+          : setMinWidthGV(lengthMin)
+      }
     }
   }
 
-  useEffect(()=>{
-    regularSection()
-  },[consumption, widthSection,requiredSpeed])
+  const onRequiredSpeed = () => {
+    if (+minWidthGV === 0) {
+      return setRequiredSpeed(0)
+    } else {
+      const requiredSpeed = (consumption * 1000 / (3600 * minWidthGV * (widthSection / 1000))).toFixed(2)
+      consumption && widthSection && minWidthGV && setRequiredSpeed(requiredSpeed)
+    }
+  }
+
+  useEffect(() => {
+    focus === 'speed' && onMinWidthGV()
+  }, [consumption, widthSection, requiredSpeed])
+
+
+  useEffect(() => {
+    focus === 'minWidth' && onRequiredSpeed()
+  }, [minWidthGV])
+
 
   return (
-    <div className="App bg">
-      <header>
+    <div className="App">
+
+      <header className='header'>
         <div className='container'>
           <div className="header__inner">
             <div className='logo'>
@@ -37,11 +57,11 @@ function App() {
         </div>
       </header>
 
-      <main>
-        <div className='container'>
-          <div className="main__inner">
+      <main className='main'>
+        <div className="main__inner">
 
-            <div className='tabs'>
+          <div className='tabs'>
+            <div className="container">
               <div className='tab tab-active'>
                 Расчёт гибкой вставки
               </div>
@@ -49,8 +69,10 @@ function App() {
                 Расчёт смесителя
               </div>
             </div>
+          </div>
 
-            <div className='main__screen'>
+          <div className='main__screen'>
+            <div className="container">
               {/* расчёт гибких вставок */}
               <div className='solution__GV'>
                 {/* расчёт в обычной секции */}
@@ -61,21 +83,21 @@ function App() {
                       <h3 className='form__title'>Расчёт в обычной секции</h3>
                       <label className='form__label label'>
                         Расход (м³):
-                        <input 
+                        <input
                           className='form__input input consumption'
                           type="number"
                           value={consumption}
-                          onChange={(e)=>setConsumption(e.target.value)}
-                          />
+                          onChange={(e) => setConsumption(e.target.value)}
+                        />
                       </label>
                       <label className='form__label label'>
                         Ширина сечения (мм):
-                        <input 
+                        <input
                           className='form__input input width_section'
                           type="number"
                           value={widthSection}
-                          onChange={(e)=>setWidthSection(e.target.value)}
-                          />
+                          onChange={(e) => setWidthSection(e.target.value)}
+                        />
                       </label>
                       <label className='form__label label'>
                         Необходимая скорость (м/с):
@@ -83,15 +105,30 @@ function App() {
                           className='form__input input required_speed'
                           type="number"
                           value={requiredSpeed}
-                          onChange={(e)=>setRequiredSpeed(e.target.value)}
-                          />
+                          onChange={(e) => {
+                            setFocus('speed')
+                            setRequiredSpeed(e.target.value)
+                          }}
+                        />
+                      </label>
+                      <label className='form__label label'>
+                        Min длина<b>Гибкой вставки</b>(мм):
+                        <input
+                          className='form__input input minWidthGV'
+                          type="number"
+                          value={minWidthGV}
+                          onChange={(e) => {
+                            setFocus('minWidth')
+                            setMinWidthGV(e.target.value)
+                          }}
+                        />
                       </label>
                     </form>
-                    <div className='result'>
+                    {/* <div className='result'>
                       Min длина <b>Гибкой вставки:</b>
-                      <br /> 
+                      <br />
                       <span>{resultRegularSection}</span>
-                    </div>
+                    </div> */}
                   </div>
 
                   <div className='img'>
@@ -104,22 +141,46 @@ function App() {
                 {/* расчёт в секции вентилятора*/}
                 {/* <section className='section section__fan'>
 
-                  <form className='form'>
-                    <h3 className='form__title'>Расчёт в секции вентилятора</h3>
-                    <label className='label'>
-                      Расход:
-                      <input className='input consumption' type="number" />
-                    </label>
-                  </form>
-                  <div>
-                    Max длина <b>Гибкой вставки:</b>
-                    <br /> 
-                    {`resultFanSection`}
+                  <div className='form-block'>
+                    <form className='form'>
+                      <h3 className='form__title'>Расчёт в секции вентилятора</h3>
+                      <label className='form__label label'>
+                        Расход (м³):
+                        <input
+                          className='form__input input consumption'
+                          type="number"
+                          value={consumption}
+                          onChange={(e) => setConsumption(e.target.value)}
+                        />
+                      </label>
+                      <label className='form__label label'>
+                        Ширина сечения (мм):
+                        <input
+                          className='form__input input width_section'
+                          type="number"
+                          value={widthSection}
+                          onChange={(e) => setWidthSection(e.target.value)}
+                        />
+                      </label>
+                      <label className='form__label label'>
+                        Необходимая скорость (м/с):
+                        <input
+                          className='form__input input required_speed'
+                          type="number"
+                          value={requiredSpeed}
+                          onChange={(e) => setRequiredSpeed(e.target.value)}
+                        />
+                      </label>
+                    </form>
+                    <div className='result'>
+                      Min длина <b>Гибкой вставки:</b>
+                      <br />
+                      <span>{resultRegularSection}</span>
+                    </div>
                   </div>
 
-
                   <div className='img'>
-                    <img src={fan} alt="Секция вентилятора" />
+                    <img src={regular} alt="обычная секция" />
                   </div>
                 </section> */}
 
@@ -151,12 +212,12 @@ function App() {
               </div> */}
 
             </div>
-
           </div>
+
         </div>
       </main>
 
-      <footer>
+      <footer className='footer'>
         <div className='container'>
           <div className="footer__inner">
             footer
