0550d28a7e9b989db43a8c8070bedc20b43445a2
Denis(denis88k) <denis88.k@mail.ru>
2023-10-26 17:28:36 +0300

    add: slider in app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,19 +1,36 @@
 import { GVPage } from 'pages/GV';
 import { MixerPage } from 'pages/Mixer';
-import { Suspense, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { Loader } from 'shared/ui/Loader/Loader';
 import { Footer } from 'widgets/Footer/Footer';
 import { Tabs } from 'widgets/Tabs/Tabs';
 
 function App() {
 	const [tab, setTab] = useState('gv');
+	const [slide, setSlide] = useState(1);
+	const images = ['1', '2', '3'];
 
 	const onTabSelect = tabName => {
 		setTab(tabName);
 	};
 
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setSlide(prevIndex => (prevIndex + 1) % images.length);
+		}, 10_000);
+
+		return () => clearInterval(interval);
+	}, []);
+
+	const style = {
+		backgroundImage: `url(src/shared/assets/img/${String(slide)}.png)`,
+	};
+
 	return (
-		<div className='App'>
+		<div
+			className='App'
+			style={style}
+		>
 			<Tabs
 				onTabSelect={onTabSelect}
 				tabSelect={tab}
