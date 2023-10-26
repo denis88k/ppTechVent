1375e32aa0866527d03b48d203cb5ec37c9186c6
Denis(denis88k) <denis88.k@mail.ru>
2023-10-26 08:58:45 +0300

    add: base url for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -6,20 +6,15 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [react()],
 	// root: './',
-	// base: '/ppTechVent/',
-	// base: '/./',
-	// publicDir: './src/',
-	// publicDir: './',
+	base: '/ppTechVent/',
 	resolve: {
 		alias: {
-			// src: '/src',
 			src: path.resolve(__dirname, './src'),
-			// src: '*/',
-			app:  path.resolve(__dirname, './src/app'),
-			pages:  path.resolve(__dirname, './src/pages'),
-			shared:  path.resolve(__dirname, './src/shared'),
-			entities:  path.resolve(__dirname, './src/entities'),
-			widgets: path.resolve(__dirname, './src/widgets')
+			app: path.resolve(__dirname, './src/app'),
+			pages: path.resolve(__dirname, './src/pages'),
+			shared: path.resolve(__dirname, './src/shared'),
+			entities: path.resolve(__dirname, './src/entities'),
+			widgets: path.resolve(__dirname, './src/widgets'),
 		},
 	},
 });
